3bb3ae2d3df1e0115c9044eaecb5e0181619c18c
Jared Haralson <jharalson29@gmail.com>
2022-04-05 19:10:57 -0400

    Updating typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,7 +22,39 @@ const typeDefs = gql`
       description: String
       image: String
       price: Number
-      category: {Category}
+      category: Category
+  }
+
+  type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+  }
+
+  type Checkout {
+    session: ID
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    categories: [Category]
+    products(category: ID, name: String) : [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+  }
+
+  type Mutation {
+    addUser(userName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(userName: String, email: String, password: String): User
+    updateProduct(_id: ID!, price: Int!): Product
+    login(email: String!, password: String!): Auth
   }
   `;
 
