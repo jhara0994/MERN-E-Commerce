8986567826044767db87062d5e665b92447bd4ed
Jared Haralson <jharalson29@gmail.com>
2022-04-05 17:52:43 -0400

    Base queries on typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    avatarUrl: String
+    orders: [Order]!
+    catalog: [Product]!
+  }
+
+  type Category {
+      _id: ID
+      name: String
+  }
+
+  type Product {
+      _id: ID
+      title: String
+      description: String
+      image: String
+      price: Number
+      category: {Category}
+  }
+
+  
+  `
