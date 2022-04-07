7baef538d5edc1bbf49b8482aa4998761627a16c
Aubrey Crook <aubreycrook@hotmail.com>
2022-04-06 21:26:14 -0400

    added test sample code

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,8 @@
+
+export function Footer (props) {
+    return (
+        <footer>
+            This is the footer
+        </footer>
+    )
+}
--- client/src/header.js
+++ client/src/header.js
@@ -1,7 +1,7 @@
 export function Header(props) {
     return (
         <header>
-            This is the header 
+            Home
         </header>
     )
 }
