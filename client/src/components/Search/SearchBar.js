d842ff10b89a60d933f5da52cbff2c26b3b1c33e
Jared Haralson <jharalson29@gmail.com>
2022-04-11 14:58:19 -0400

    SearchBar is semi-working

--- client/src/components/Search/SearchBar.js
+++ client/src/components/Search/SearchBar.js
@@ -1,41 +1,87 @@
-import React, { useEffect, useState } from 'react';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { useStoreContext } from '../../utils/GlobalState';
-import { useLazyQuery } from '@apollo/client';
-import ProductItem from '../ProductItem/ProductItem';
-import { SEARCH_BAR } from '../../utils/actions';
+import React, { useState } from 'react';
+// import {SearchIcon} from '@mui/material';
+// import {CloseIcon} from '@mui/material';
 
-function SearchBar(item) {
-    const [state, dispatch] = useStoreContext();
-  
-    const { search } = state;
-  
-    const [ queryProducts ] = useLazyQuery(QUERY_PRODUCTS);
+// import { QUERY_PRODUCTS } from '../../utils/queries';
+// import { useStoreContext } from '../../utils/GlobalState';
+// import { useLazyQuery } from '@apollo/client';
+// import ProductItem from '../ProductItem/ProductItem';
+// import { SEARCH_BAR } from '../../utils/actions';
+// import { idbPromise } from '../../utils/helpers';
 
-    const {
-        title,
-        description,
-        _id
-      } = item;
+function SearchBar({item}) {
+    const [filteredData, setFilteredData] = useState([])
+    const [titleEntered, setTitleEntered] =useState("")
 
-    function submitSearch() {
-        const searchInput = search.findAll((products) => products.title === title)
-        if(searchInput != title) {
-            alert('Please enter a different product title')
+    const handleFilter = (e) => {
+        const searchTitle = e.target.value
+        setTitleEntered(searchTitle)
+        const newFilter = item.filter((value) => {
+            return value.title.toLowerCase().includes(searchTitle.toLowerCase())
+        })
+
+        if (searchTitle === "") {
+            setFilteredData([])
         } else {
-            dispatch({
-                type: SEARCH_BAR,
-                products: { ...item}
-            })
+            setFilteredData(newFilter)
         }
-
     }
+
+    const clearInput = () => {
+        setFilteredData([])
+        setTitleEntered("")
+    }
+
+    // const [state, dispatch] = useStoreContext();
+  
+    // const { search } = state;
+  
+    // const [ queryProducts ] = useLazyQuery(QUERY_PRODUCTS);
+
+    // const {
+    //     title,
+    //     description,
+    //     _id
+    //   } = item;
+
+    // function submitSearch() {
+    //     const searchInput = search.findAll((products) => products.title === title)
+    //     if(searchInput !== title) {
+    //         alert('Please enter a different product title')
+    //     } else {
+    //         dispatch({
+    //             type: SEARCH_BAR,
+    //             products: { ...item}
+    //         })
+    //         idbPromise('product', 'get', { ...item})
+    //     }
+
+    // }
 
 
     return (
         <div className="search-container">
-            <input type='text' className="search-input" placeholder="Search products"></input>
-            <button onClick={submitSearch} type='submit' className="search-btn">Search</button>
+            <div className="search-input-container">
+                <input type='text' className="search-input" placeholder="Search products" value={titleEntered} onChange={handleFilter} />
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? (
+                        <div> </div>
+                    ) : (
+                        <span id="clear-btn" onClick={clearInput} />
+                    )}
+                </div>
+            </div>
+            {filteredData.length !== 0 && (
+                <div className="data-result">
+                    {filteredData.slice(0, 15).map((value, key) => {
+                        return (
+                            <a className="data-item" href={value.link} target="_blank" rel="noreferrer">
+                                <p>{value.title}</p>
+                            </a>
+                        )
+                    })}
+                </div>
+            )}
         </div>
     )
 }
