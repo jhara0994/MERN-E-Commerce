89296d57812893b84ef3337815a8d0257694428b
Jared Haralson <jharalson29@gmail.com>
2022-04-12 21:50:25 -0400

    Adding photos so we can develop css

--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -1,44 +1,40 @@
 import React from "react";
 import { Link, Router } from "react-router-dom";
-import { pluralize } from "../../utils/helpers"
+import { pluralize } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { ADD_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import css from './Product.module.css'
+import css from "./Product.module.css";
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
 
-  const {
-    image,
-    title,
-    description,
-    _id,
-    price
-  } = item;
+  const { image, title, description, _id, price } = item;
 
-  const { cart } = state
+  const { cart } = state;
 
   const addToCart = () => {
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
     if (!itemInCart) {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...item, purchaseQuantity: 1 }
+        product: { ...item, purchaseQuantity: 1 },
       });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-    } 
-  }
+      idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
+    }
+  };
+
+  console.log(image);
 
   return (
     <div className={css.card}>
       {/* <Link to='/products/:id' */}
-        <h3>{title}</h3>
-        <img
-          alt={title}
-          src={`/images/${image}`}
-        />
-        <p>{description}</p>
+      <h3>{title}</h3>
+      {image && (
+        <img alt={title} src={require(`../../assets/images/${image}`)} />
+      )}
+
+      <p>{description}</p>
       {/* </Link> */}
       <div>
         <div>1 item in stock</div>
@@ -48,6 +44,5 @@ function ProductItem(item) {
     </div>
   );
 }
-
 
 export default ProductItem;
