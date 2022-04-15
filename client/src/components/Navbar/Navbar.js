b63f813240b60237f889f274ad2e4f3ab18c1da2
Aubrey Crook <aubreycrook@hotmail.com>
2022-04-14 20:47:39 -0400

    Pushing changes.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,6 +6,7 @@ import SearchBar from '../Search/SearchBar'
 const Navbar = () => {
     return (
         <div className={classes.Navbar}>
+            <SearchBar />
             <LargeNav />
             <MobileNav />
         </div>
