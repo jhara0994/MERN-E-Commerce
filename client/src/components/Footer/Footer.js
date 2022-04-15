caf86146ec9fdd524c436febc196918d07e4232a
Jared Haralson <jharalson29@gmail.com>
2022-04-15 12:55:03 -0400

    Added phone icon on footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import css from './Footer.module.css'
+import PhoneIcon from '@mui/icons-material/Phone';
 
 export default function Footer(props) {
     return (
         <footer>
           <div className={css.footerInfo}>
-            <a href="tel:+4045558899" className="contact-info">Phone</a>
+            <a href="tel:+4045558899" className="contact-info"><PhoneIcon /></a>
             <a href="mailto: jharalson29@gmail.com?subject=Art E-Commerce" className="contact-info">Email</a>
             <a href="https://github.com/jhara0994" className="contact-info">Github</a>
           </div>
